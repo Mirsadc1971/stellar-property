c7935dc4c02fde184914f89b16450c2ec9d469f4
Mirsadc1971 <mirsadc1971@icloud.com>
2025-06-21 22:12:48 -0500

    Fix All Community Pages

--- src/pages/communities/RogersPark.tsx
+++ src/pages/communities/RogersPark.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import CommunityLayout from '@/components/community/CommunityLayout';
 
 export default function RogersPark() {
